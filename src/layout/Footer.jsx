9a1b98234fa6bb4ea94d7aa9e0d6baab444a9877
BarbossaH <67042094+BarbossaH@users.noreply.github.com>
2024-10-05 10:09:00 +1300

    adjust the layout of footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="max-w-screen-xl mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="flex flex-col items-start text-center md:order-1">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="flex flex-col space-y-4 text-center md:text-left">
             <h2 className="text-3xl font-bold">Forest Group</h2>
             <p className="text-md text-gray-400">
               Building a sustainable future together.
@@ -17,10 +17,10 @@ const Footer = () => {
             </p>
           </div>
 
-          <div className="flex flex-col space-y-4 md:order-2">
+          <div className="flex flex-col space-y-6">
             <h3 className="text-2xl font-bold">Contact Us</h3>
 
-            <div className="flex items-center space-x-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="flex items-center">
                 <FaEnvelope className="mr-2 text-blue-400" />{" "}
                 <a
@@ -36,14 +36,18 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="flex items-center">
-              <FaPhoneAlt className="mr-2 text-red-500" />
-              <span className="text-gray-300 text-lg">0220899308</span>
-              <span className="mx-2 text-gray-300 text-lg">|</span>
-              <span className="text-gray-300 text-lg">0211065271</span>
-              <span className="ml-2 text-gray-300 text-lg font-semibold">
-                Contact: Shawn Fei
-              </span>
+            <div className="grid grid-cols-1 gap-2">
+              <div className="flex flex-col md:flex-row items-start md:items-center">
+                <div className="flex items-center">
+                  <FaPhoneAlt className="mr-2 text-red-500" />
+                  <span className="text-gray-300 text-lg">0220899308</span>
+                  <span className="mx-2 text-gray-300 text-lg">|</span>
+                  <span className="text-gray-300 text-lg">0211065271</span>
+                </div>
+                <span className="ml-0 md:ml-4 mt-2 md:mt-0 text-gray-300 text-lg font-semibold">
+                  Contact: Shawn Fei
+                </span>
+              </div>
             </div>
           </div>
         </div>
