ee0f7a062cf8555bf5c653852cb1b0b179f48468
Moa Ha <moahaa03@gmail.com>
2024-10-15 21:54:02 +1300

    font-mono to h tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Arial", "Roboto", "Montserrat", "Palanquin", "sans-serif"],
+        sans: ['Arial', 'Roboto', 'Montserrat', 'Palanquin', 'sans-serif'],
+        mono: ['Courier New', 'Menlo', 'Monaco', 'Consolas', 'monospace'],
       },
       animation: {
-        fadeInOut: "fadeInOut 10s ease-in-out infinite",
+        fadeInOut: 'fadeInOut 10s ease-in-out infinite',
       },
       keyframes: {
         fadeInOut: {
-          "0%, 100%": { opacity: "0" },
-          "10%, 90%": { opacity: "1" },
+          '0%, 100%': { opacity: '0' },
+          '10%, 90%': { opacity: '1' },
         },
       },
     },
   },
   plugins: [],
-};
+}
