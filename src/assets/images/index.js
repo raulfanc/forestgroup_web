5fada03c9ab3e8738f70e5d3eeb88182610af20a
Moa Ha <moahaa03@gmail.com>
2024-10-15 16:05:52 +1300

    location images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,27 +1,29 @@
-import logo from "./logo.svg";
-import background2 from "./background2.jpg";
-import background3 from "./background3.png";
-import background4 from "./background4.png";
-import background5 from "./background5.png";
-import background6 from "./background6.png";
-import background7 from "./background7.png";
-import background8 from "./background8.png";
-import background9 from "./background9.png";
-import background10 from "./background10.png";
-import background11 from "./background11.png";
-import background12 from "./background12.png";
-import background13 from "./background13.png";
-import background14 from "./background14.png";
-import background15 from "./background15.png";
-import background16 from "./background16.png";
-import background17 from "./background17.png";
-import background18 from "./background18.png";
-import background19 from "./background19.png";
-import background20 from "./background20.png";
-import location from "./location.png";
-import company1 from "./company1.png";
-import company2 from "./company2.png";
-import company3 from "./company3.png";
+import logo from './logo.svg'
+import background2 from './background2.jpg'
+import background3 from './background3.png'
+import background4 from './background4.png'
+import background5 from './background5.png'
+import background6 from './background6.png'
+import background7 from './background7.png'
+import background8 from './background8.png'
+import background9 from './background9.png'
+import background10 from './background10.png'
+import background11 from './background11.png'
+import background12 from './background12.png'
+import background13 from './background13.png'
+import background14 from './background14.png'
+import background15 from './background15.png'
+import background16 from './background16.png'
+import background17 from './background17.png'
+import background18 from './background18.png'
+import background19 from './background19.png'
+import background20 from './background20.png'
+import location from './location.png'
+import company1 from './company1.png'
+import company2 from './company2.png'
+import company3 from './company3.png'
+import nz_map from './nz_map.png'
+import nz_map_name from './nz_map_name.png'
 
 export {
   logo,
@@ -48,4 +50,6 @@ export {
   company1,
   company2,
   company3,
-};
+  nz_map,
+  nz_map_name,
+}
