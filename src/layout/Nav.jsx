2b801ec607d36f23fdf65e0622b61a7f23c1fc4d
Moa Ha <moahaa03@gmail.com>
2024-10-21 00:11:09 +1300

    navbar menu

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -79,26 +79,30 @@ const Nav = () => {
     <header className="fixed z-10 w-full">
       {/* <nav className="flex items-center justify-between bg-white p-2"> */}
       {/* for colored backgground for nav bar */}
-      <nav className="shadow-md flex items-center justify-between bg-[#eae0d5] p-2 ">
+      <nav className="uppercase shadow-md flex items-center justify-between bg-[#eae0d5] p-2">
         <div className="flex items-center">
           <NavLink to="/">
             <img src={logo3} alt="Logo" className="max-w-[110px] h-auto pl-8" />
           </NavLink>
           <NavLink to="/">
             {' '}
-            <span className="font-audiowide pl-4">FOREST GROUP</span>
+            <span className="font-audiowide pl-4 text-[18px]">
+              FOREST GROUP
+            </span>
           </NavLink>
         </div>
 
-        <div className="flex items-center space-x-4">
-          <ul className="flex-1 flex justify-end items-end gap-16 max-lg:hidden pr-12">
+        <div>
+          <ul className="flex-1 flex justify-end gap-8 max-lg:hidden pr-4 text-[15px]">
             {navLinks.map((item) => (
-              <li key={item.label} className="hover:text-[#085332] ">
+              <li key={item.label}>
                 <NavLink
                   to={item.href}
                   className={({ isActive }) =>
                     `font-audiowide  ${
-                      isActive ? 'text-[#085332]' : 'text-black'
+                      isActive
+                        ? 'text-[#085332]'
+                        : 'text-black hover:text-[#085332]'
                     }`
                   }
                 >
@@ -120,14 +124,14 @@ const Nav = () => {
         </div>
       </nav>
       {isOpenMenu && (
-        <div className="mt-4 absolute top-20 left-0 w-full bg-[#eae0d5] transition-colors rounded-lg shadow-lg">
+        <div className="uppercase font-audiowide mt-4 absolute top-20 w-full bg-[#eae0d5] transition-colors rounded-lg shadow-lg">
           <ul>
             {navLinks.map((item) => (
               <li key={item.label}>
                 <NavLink
                   to={item.href}
                   className={({ isActive }) =>
-                    `shadow-lg text-lg pl-8 pb-2 pt-2 block transition-colors duration-200 ${
+                    `shadow-lg text-[15px] pl-12 p-4 block transition-colors duration-200 ${
                       isActive ? 'text-[#085332]' : 'text-black'
                     } hover:bg-[#d6c6b9] rounded-lg`
                   }
