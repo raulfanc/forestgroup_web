a9d12d2cd9914c220263f3f74fc4e1d48ebcfd98
BarbossaH <67042094+BarbossaH@users.noreply.github.com>
2024-10-22 08:27:52 +1300

    adjust the font size of slogan twice

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,18 +6,18 @@ import Button from "../components/Button";
 import { FaPhone } from "react-icons/fa";
 
 const content = (
-  <div className="relative h-full text-white grid grid-rows-1 grid-cols-1">
-    <div className="flex items-start justify-center p-12 row-start-1 col-start-1">
-      <h1 className="mt-8 text-center text-2xl sm:text-4xl lg:text-5xl leading-tight sm:leading-normal lg:leading-normal px-4 sm:px-0 bg-black bg-opacity-40 p-4 rounded-md lg:bg-transparent">
-      Complex civil specialist, we deliver efficiency and quality to your
-        projects.
-      </h1>
-    </div>
-
-    <div className="absolute bottom-8 right-8">
-      <Button label="Call Us Now" phoneNumber="0220899308" icon={<FaPhone />} />
-    </div>
+<div className="relative h-full text-white grid grid-rows-1 grid-cols-1">
+  <div className="flex items-start justify-center p-12 sm:px-2 row-start-1 col-start-1">
+    <h1 className="mt-8 text-center text-2xl sm:text-4xl lg:text-5xl leading-tight sm:leading-normal lg:leading-normal px-4 sm:px-2 bg-black bg-opacity-40 p-4 rounded-md lg:bg-transparent">
+      Complex civil specialist, we deliver efficiency and quality to your projects.
+    </h1>
   </div>
+
+  <div className="absolute bottom-8 right-8">
+    <Button label="Call Us Now" phoneNumber="0220899308" icon={<FaPhone />} />
+  </div>
+</div>
+
 );
 
 const HomePage = () => {
